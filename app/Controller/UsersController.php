c042c2b4420356e703da23955b90dc44d2801bf2
Vinicius <vinicius@ubuntu.ubuntu-domain>
2013-07-16 15:47:32 -0300

    Criaçao do add.ctp de users

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -4,7 +4,7 @@ class UsersController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('logout');
+        $this->Auth->allow('add', 'logout');
     }
 
     public function index() {
@@ -15,25 +15,54 @@ class UsersController extends AppController {
     public function view($id = null) {
         $this->User->id = $id;
         if (!$this->User->exists()) {
-            throw new NotFoundException(__('Usuário inválido!'));
+            throw new NotFoundException(__('Invalid user'));
         }
         $this->set('user', $this->User->read(null, $id));
     }
 
-    public function login() {
-        if ($this->Auth->login()) {
-            debug('oi');
-            exit();
-            $this->redirect($this->Auth->redirect());
-        } else {
-            debug('opa! falhou');
-            exit();
-            $this->Session->setFlash(__('Usuário ou senha inválidos, tente novamente.'));
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->User->create();
+            if ($this->User->save($this->request->data)) {
+                $this->Session->setFlash(__('The user has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The user could not be saved. Please, try again.'));
+            }
         }
     }
 
-    public function logout() {
-        $this->redirect($this->Auth->logout());
+    public function edit($id = null) {
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->User->save($this->request->data)) {
+                $this->Session->setFlash(__('The user has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The user could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->User->read(null, $id);
+            unset($this->request->data['User']['password']);
+        }
     }
-}
+
+    public function delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        if ($this->User->delete()) {
+            $this->Session->setFlash(__('User deleted'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('User was not deleted'));
+        $this->redirect(array('action' => 'index'));
+    }
 ?>
