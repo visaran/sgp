69a170adab896a818fe15a08f97672f100fa4fbd
Tales <tales@ubuntu.ubuntu-domain>
2013-07-18 17:39:12 -0300

    Adiçao de arquivo ReservationTest.php

--- app/Controller/ReservationsController.php
+++ app/Controller/ReservationsController.php
@@ -12,7 +12,7 @@ class ReservationsController extends AppController {
 		//if ($this->request->is('reservation')) {
         	//$this->request->data['Reservation']['user_id'] = $this->Auth->user('id'); //Added this line
         if (!empty($this->data)) {
-        	$this->data['Reservation']['user_id'] = $this->Auth->user('id');
+        	$this->request->data['Reservation']['user_id'] = $this->Auth->user('id');
         	//debug($this->Auth->user('id')); exit();
         	//debug($this->data);exit();
             $this->Reservation->save($this->data);
