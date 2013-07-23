69c0b081570cd1eccd33d3e9bc6de12ad43541ca
vanessasouzagabriel <nessagabriel@hotmail.com>
2013-07-23 16:58:06 -0300

    Ateraçao de assertTrue pra assertFalse no testReservationLimiteProjetoresMaiorQueLimite

--- app/Test/Case/Model/ReservationTest.php
+++ app/Test/Case/Model/ReservationTest.php
@@ -59,7 +59,7 @@ class ReservationTest extends CakeTestCase{
 
 	function testReservationLimiteProjetoresMaiorQueLimite() {
 		$reserva = array('Reservation' => array('horario_reserva_1' => 1, 'horario_reserva_2' => 0, 'data_reserva' => '23/07/2013'));
-		$this->assertTrue($this->Reservation->validaLimiteReservas($reserva, 3));
+		$this->assertFalse($this->Reservation->validaLimiteReservas($reserva, 3));
 	}
 
 	
